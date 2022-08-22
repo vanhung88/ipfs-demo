df9738d9fd0646f972f57924a6d847bfd024ff5f
hung.le@team.enouvo.com <88levanhung@gmail.com>
2022-08-22 23:42:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IPFS DEMO</h1>
+      <img src="./img1.png" style={{ height: "80vh"}}/>
     </div>
   );
 }
